095072d83e34e2599ef74dacd196b6e09a5172bb
Laurent Egbakou <26142591+egbakou@users.noreply.github.com>
2022-02-23 20:50:13 +0100

    feat: added morgan config

---
+++ src/middlewares/morgan.middleware.js
@@ -0,0 +1,25 @@
+const morgan = require("morgan");
+const logger = require("../utils/logger");
+
+const stream = {
+  // Use the http severity
+  write: (message) => logger.http(message),
+};
+
+const skip = () => {
+  const env = process.env.NODE_ENV || "development";
+  return env !== "development";
+};
+
+const morganMiddleware = morgan(
+  // Define message format string (this is the default one).
+  // The message format is made from tokens, and each token is
+  // defined inside the Morgan library.
+  // You can create your custom token to show what do you want from a request.
+  ":remote-addr :method :url :status :res[content-length] - :response-time ms",
+  // Options: in this case, I overwrote the stream and the skip logic.
+  // See the methods above.
+  { stream, skip }
+);
+
+module.exports = morganMiddleware;
